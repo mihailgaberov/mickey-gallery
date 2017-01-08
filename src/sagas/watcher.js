cb1d1b74e95657f0936dc660240d62e5b0eee7ae
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-01-08 13:59:59 +0100

    Update takeLatest helper usage.

--- src/sagas/watcher.js
+++ src/sagas/watcher.js
@@ -1,11 +1,11 @@
 /**
  * Created by Mihail on 1/7/2017.
  */
-import { takeLatest } from 'redux-saga';
+import { takeLatest } from 'redux-saga/effects';
 import { searchMediaSaga } from './mediaSaga';
 import * as types from '../constants/actionTypes';
 
 // Watches for SEARCH_MEDIA_REQUEST action type asynchronously
 export default function* watchSearchMedia() {
-	yield* takeLatest(types.SEARCH_MEDIA_REQUEST, searchMediaSaga);
+	yield takeLatest(types.SEARCH_MEDIA_REQUEST, searchMediaSaga);
 }
