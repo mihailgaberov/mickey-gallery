9d61f4ff504ad35f9625a8e009f67c86be1fa8eb
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-01-08 12:59:43 +0100

    Configured the app entry point to use Redux store.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 import routes from './routes';
 
-// We require the routes and render to the DOM using ReactDOM API
+// Initialize store
+const store = configureStore();
+
 ReactDOM.render(
-	<Router history={browserHistory} routes={routes} />,
+	<Provider store={store}>
+		<Router history={browserHistory} routes={routes} />
+	</Provider>,
 	document.getElementById('root')
 );
