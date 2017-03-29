f4eda7728906821bc926eaac60672b1ecc9f09d7
Mihail Gaberov <mihail.gaberov@dreamit.de>
2017-03-29 22:24:20 +0200

    Added test for Video Reducer component.

--- src/reducers/videoReducer.js
+++ src/reducers/videoReducer.js
@@ -4,8 +4,6 @@
 import initialState from './initialState';
 import * as types from '../constants/actionTypes';
 
-// Handles video related actions
-// The idea is to return an updated copy of the state depending on the action type.
 export default function (state = initialState.videos, action) {
 	switch (action.type) {
 		case types.SHUTTER_VIDEOS_SUCCESS:
---
+++ test/videoReducer.test.js
@@ -0,0 +1,30 @@
+/**
+ * Created by mgab on 29/03/2017.
+ */
+import expect from 'expect';
+import videoReducer from '../src/reducers/videoReducer';
+import * as types from '../src/constants/actionTypes';
+
+describe('Test for Video Reducer', () => {
+  const initialState = {
+    videos: [{
+      id: 1,
+      link: 'www.test.com/1.mp4'
+    }]
+  };
+
+  it('should return the initial state', () => {
+    expect(videoReducer(undefined, [])).toEqual([]);
+  });
+
+  it('should return the all videos in the stores tree', () => {
+    const testAction = { type: types.SHUTTER_VIDEOS_SUCCESS, videos: 'www.test.com/1.mp4' };
+    expect(videoReducer(initialState, testAction)).toEqual([ 'www.test.com/1.mp4' ]);
+  });
+
+  it('should return the selected video', () => {
+    const testAction = { type: types.SELECTED_VIDEO, video: 'www.test.com/1.mp4' };
+    const expectValue = { videos: [{ id: 1, link: 'www.test.com/1.mp4' }], selectedVideo: 'www.test.com/1.mp4' };
+    expect(videoReducer(initialState, testAction)).toEqual(expectValue);
+  });
+});
