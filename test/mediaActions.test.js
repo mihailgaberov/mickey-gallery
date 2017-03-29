f29453cc430810ed9b7174174fc25c27ad5f58ed
Mihail Gaberov <mihail.gaberov@dreamit.de>
2017-03-29 21:51:57 +0200

    Added test for Media Actions.

---
+++ test/mediaActions.test.js
@@ -0,0 +1,29 @@
+/**
+ * Created by mgab on 29/03/2017.
+ */
+import expect from 'expect';
+import {
+  selectImageAction,
+  searchMediaAction,
+  selectVideoAction
+} from '../src/actions/mediaActions';
+import * as types from '../src/constants/actionTypes';
+
+
+describe('Test for Action creators', () => {
+
+  it('should return selected images action object', () => {
+    const image = { id: 1, link: 'great.com/1.jpg' };
+    expect(selectImageAction(image)).toEqual({ type: types.SELECTED_IMAGE, image });
+  });
+
+  it('should return selected video action object', () => {
+    const video = { id: 1, link: 'great.com/1.mp4' };
+    expect(selectVideoAction(video)).toEqual({ type: types.SELECTED_VIDEO, video });
+  });
+
+  it('should return searchMediaAction action object', () => {
+    const test = { id: 1, link: 'great.com/1.jpg' };
+    expect(searchMediaAction(test)).toEqual({ type: types.SEARCH_MEDIA_REQUEST, payload: test });
+  });
+});
