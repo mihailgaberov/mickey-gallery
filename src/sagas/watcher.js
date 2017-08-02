a186f83d8a3b7ae4a2257be88c0a3968621988e0
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-08-03 01:32:35 +0200

    Fixed deprecated packages warnings.

--- src/sagas/watcher.js
+++ src/sagas/watcher.js
@@ -6,5 +6,5 @@ import searchMediaSaga from './mediaSaga'
 import * as types from '../constants/actionTypes'
 
 export default function* watchSearchMedia() {
-	yield* takeLatest(types.SEARCH_MEDIA_REQUEST, searchMediaSaga)
+	yield takeLatest(types.SEARCH_MEDIA_REQUEST, searchMediaSaga)
 }
