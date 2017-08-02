cbcf65fa9a98f00157952b921568e06ce022ebd5
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-08-03 01:13:21 +0200

    Fixed deprecated packages warnings.

--- src/sagas/watcher.js
+++ src/sagas/watcher.js
@@ -1,10 +1,10 @@
 /**
  * Created by Mihail on 1/7/2017.
  */
-import { takeLatest } from 'redux-saga';
-import searchMediaSaga from './mediaSaga';
-import * as types from '../constants/actionTypes';
+import { takeLatest } from 'redux-saga/effects'
+import searchMediaSaga from './mediaSaga'
+import * as types from '../constants/actionTypes'
 
 export default function* watchSearchMedia() {
-	yield* takeLatest(types.SEARCH_MEDIA_REQUEST, searchMediaSaga);
+	yield* takeLatest(types.SEARCH_MEDIA_REQUEST, searchMediaSaga)
 }
