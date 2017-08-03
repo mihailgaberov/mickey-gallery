c7e82445e5e54c5f048f5d2d8fea2f33c4c87fc1
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-08-03 10:59:01 +0200

    Fixed deprecated warnings.

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -4,9 +4,6 @@
 import { fork } from 'redux-saga/effects';
 import watchSearchMedia from './watcher';
 
-// Here we register our watcher saga(s) and export as a single generator
-// function (startForeman) as our root Saga.
-
-export default function* startForeman () {
-	yield fork(watchSearchMedia);
+export default function* startForeman() {
+  yield fork(watchSearchMedia);
 }
--- src/sagas/mediaSaga.js
+++ src/sagas/mediaSaga.js
@@ -1,21 +1,25 @@
 /**
  * Created by Mihail on 1/7/2017.
  */
-import { put, call } from 'redux-saga/effects'
+import { put, call, all } from 'redux-saga/effects'
 import { flickrImages, shutterStockVideos } from '../API/api'
 import * as types from '../constants/actionTypes'
 
-export default function* searchMediaSaga({ payload }) {
-	try {
-		const videos = yield call(shutterStockVideos, payload)
-		const images = yield call(flickrImages, payload)
-		yield [
-			put({ type: types.SHUTTER_VIDEOS_SUCCESS, videos }),
-			put({ type: types.SELECTED_VIDEO, video: videos[0] }),
-			put({ type: types.FLICKR_IMAGES_SUCCESS, images }),
-			put({ type: types.SELECTED_IMAGE, image: images[0] })
-		]
-	} catch (error) {
-		yield put({ type: 'SEARCH_MEDIA_ERROR', error })
-	}
+export default function* searchMediaSaga({payload}) {
+  try {
+    const {videos, images} = yield all({
+      videos: call(shutterStockVideos, payload),
+      images: call(flickrImages, payload)
+    })
+    yield all(
+      [
+        put({type: types.SHUTTER_VIDEOS_SUCCESS, videos}),
+        put({type: types.SELECTED_VIDEO, video: videos[0]}),
+        put({type: types.FLICKR_IMAGES_SUCCESS, images}),
+        put({type: types.SELECTED_IMAGE, image: images[0]})
+      ]
+    )
+  } catch (error) {
+    yield put({type: 'SEARCH_MEDIA_ERROR', error})
+  }
 }
