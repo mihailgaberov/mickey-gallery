fbf721cedcbcee0b4ec5e1feb99bad4993f27fb4
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-08-05 16:23:08 +0200

    Fixed saga watcher tests.

--- test/watcher.test.js
+++ test/watcher.test.js
@@ -2,18 +2,16 @@
  * Created by mgab on 29/03/2017.
  */
 import expect from 'expect'
-import { fork, take } from 'redux-saga/effects'
+import { takeLatest } from 'redux-saga/effects'
 import searchMediaSaga from '../src/sagas/mediaSaga'
 import watchSearchMedia from '../src/sagas/watcher'
 
-xdescribe('watcher', () => {
+describe('watcher', () => {
 
   describe('watchSearchMedia', () => {
     it('should call searchMediaSaga', () => {
       const gen = watchSearchMedia()
-      const action = { type: 'SEARCH_MEDIA_REQUEST' }
-      expect(gen.next().value).toEqual(take('SEARCH_MEDIA_REQUEST'))
-      expect(gen.next(action).value).toEqual(fork(searchMediaSaga, action))
+      expect(gen.next().value).toEqual(takeLatest('SEARCH_MEDIA_REQUEST', searchMediaSaga))
     })
   })
 })
