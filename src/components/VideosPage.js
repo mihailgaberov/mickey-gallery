c00803a2440fb1c4bcc23ec173b2d7c56deecea5
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-08-06 16:25:29 +0200

    Fixed an error when trying to access selected video data before it's loaded.

--- src/components/VideosPage.js
+++ src/components/VideosPage.js
@@ -7,12 +7,13 @@ import PropTypes from 'prop-types'
 const VideosPage = ({videos, onHandleSelectVideo, selectedVideo}) => (
   <div className="col-md-6">
     <h2>Videos</h2>
-    <div className="select-video">
+    {selectedVideo ? <div className="select-video">
       <div id={selectedVideo.id}>
         <h6 className="title">{selectedVideo.title}</h6>
         <video controls src={selectedVideo.mediaUrl} alt={selectedVideo.title}/>
       </div>
-    </div>
+    </div> : ''
+    }
     <div className="video-thumbnail">
       {videos.map((video, i) => (
         <div key={i} onClick={onHandleSelectVideo.bind(this, video)}>
