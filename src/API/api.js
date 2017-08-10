f0a6bfba9b97551420b9e73ed46579af251e7715
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-08-10 12:43:42 +0200

    Reversed the arrays with the images and videos in order to show first the ones that are uploaded lastly.

--- src/API/api.js
+++ src/API/api.js
@@ -14,14 +14,15 @@ export const flickrImages = () => {
 	&nojsoncallback=1
 	&per_page=1000
 	&user_id=${REACT_APP_FLICKR_USER_ID}
-	&photoset_id=${REACT_APP_FLICKR_PHOTOSET_ID}`
+	&photoset_id=${REACT_APP_FLICKR_PHOTOSET_ID}
+	&extras=date_upload`
 
   return fetch(FLICKR_API_ENDPOINT)
     .then(response => {
       return response.json()
     })
     .then(json => {
-      return json.photoset.photo.map(({farm, server, id, secret, title}) => ({
+      return json.photoset.photo.reverse().map(({farm, server, id, secret, title}) => ({
         id,
         title,
         mediaUrl: `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`
@@ -36,14 +37,15 @@ export const flickrVideos = () => {
 	&nojsoncallback=1
 	&per_page=100
 	&user_id=${REACT_APP_FLICKR_USER_ID}
-	&photoset_id=${REACT_APP_FLICKR_VIDEOSET_ID}`
+	&photoset_id=${REACT_APP_FLICKR_VIDEOSET_ID}
+	&extras=date_upload`
 
   return fetch(FLICKR_API_ENDPOINT)
     .then(response => {
       return response.json()
     })
     .then(json => {
-      return json.photoset.photo.map(({id, secret, title}) => ({
+      return json.photoset.photo.reverse().map(({id, secret, title}) => ({
         id,
         title,
         mediaUrl: `https://www.flickr.com/photos/${REACT_APP_FLICKR_USER_ID}/${id}/play/site/${secret}`
