120186e891a1ca131d7db41979740efed8bca29b
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-10-15 23:23:20 +0200

    Fixed the height of the loader spinner.

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const imgPath = require('../images/loader.gif')
 const Spinner = styled.div`
     width: 31px;
-    height: 30px;
+    height: 31px;
     margin: 0 auto;
     background-image: url(${imgPath});
 `
