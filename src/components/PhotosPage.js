28e500542dcca23c59e1aaf2f490be32741250ff
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-10-16 16:32:17 +0200

    Refactoring: extracted the PhotoPage from the common media library page that contained both photos and videos.

--- src/components/PhotosPage.js
+++ src/components/PhotosPage.js
@@ -1,33 +1,73 @@
 /**
  * Created by Mihail on 1/8/2017.
  */
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { searchImagesAction, selectImageAction } from '../actions/mediaActions'
+import '../styles/style.css'
+import Spinner from '../components/StyledComponents/Spinner'
+import ErrorMsg from '../components/ErrorMsg'
 
-const PhotosPage = ({images, onHandleSelectImage, selectedImage}) => (
-  <div className="col-md-6">
-    <h2>Images</h2>
-    {selectedImage ?
-      <div className="selected-image">
-        <div id={selectedImage.id}>
-          <h6 className="title">{selectedImage.title}</h6>
-          <img src={selectedImage.mediaUrl} alt={selectedImage.title}/>
-        </div>
-      </div> : ''}
-    <div className="image-thumbnail">
-      {images.map((image, i) => (
-        <div key={i} onClick={onHandleSelectImage.bind(this, image)}>
-          <img src={image.mediaUrl} alt={image.title}/>
-        </div>
-      ))}
-    </div>
-  </div>
-)
+export class PhotosPage extends Component {
+  constructor() {
+    super()
+    this.handleSelectImage = this.handleSelectImage.bind(this)
+  }
 
-PhotosPage.propTypes = {
-  images: PropTypes.array.isRequired,
-  selectedImage: PropTypes.object,
-  onHandleSelectImage: PropTypes.func.isRequired
+  componentDidMount() {
+    this.props.dispatch(searchImagesAction())
+  }
+
+  handleSelectImage(selectedImage) {
+    this.props.dispatch(selectImageAction(selectedImage))
+  }
+
+  render() {
+    const {
+      images,
+      selectedImage,
+      imagesError,
+    } = this.props
+
+    return (
+      <div>
+        {!imagesError ?
+          images ?
+            <div>
+              {selectedImage ?
+                <div className="selected-image">
+                  <div id={selectedImage.id}>
+                    <h6 className="title">{selectedImage.title}</h6>
+                    <img src={selectedImage.mediaUrl} alt={selectedImage.title}/>
+                  </div>
+                </div> : ''}
+              <div className="image-thumbnail">
+                {images.map((image, i) => (
+                  <div key={i} onClick={this.handleSelectImage.bind(this, image)}>
+                    <img src={image.mediaUrl} alt={image.title}/>
+                  </div>
+                ))}
+              </div>
+            </div> : <Spinner/>
+          : <ErrorMsg>{imagesError}</ErrorMsg>
+        }
+      </div>
+    )
+  }
 }
 
-export default PhotosPage
+PhotosPage.propTypes = {
+  images: PropTypes.array,
+  selectedImage: PropTypes.object,
+  dispatch: PropTypes.func.isRequired,
+  imagesError: PropTypes.string,
+}
+
+const mapStateToProps = ({ images, error }) => ({
+  images: images[0],
+  selectedImage: images.selectedImage,
+  imagesError: images.error
+})
+
+export default connect(mapStateToProps)(PhotosPage)
