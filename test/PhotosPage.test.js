b5df456ee354f12a0d2af94f6cda9468a6044c46
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-10-22 16:19:59 +0200

    Updating the tests for Photos and Videos pages.

--- test/PhotosPage.test.js
+++ test/PhotosPage.test.js
@@ -3,12 +3,14 @@ import expect from 'expect'
 import { shallow } from 'enzyme'
 import { PhotosPage } from '../src/components/PhotosPage'
 import LazyLoad from 'react-lazyload'
+import SpinnerContainer from '../src/components/StyledComponents/SpinnerContainer'
+import Spinner from '../src/components/StyledComponents/Spinner'
 
-const setup = () => {
+const setup = (images) => {
   const props = {
     handleSearch: expect.createSpy(),
     dispatch: expect.createSpy(),
-    images: [{ id: 1, mediaUrl: 'test image url', width: 320, height: 220 }]
+    images: images
   }
 
   const Wrapper = shallow(<PhotosPage {...props} />)
@@ -17,10 +19,17 @@ const setup = () => {
 
 describe('Test for PhotosPage', () => {
   it('should render self and sub components', () => {
-    const { Wrapper } = setup()
+    const { Wrapper } = setup([{ id: 1, mediaUrl: 'test image url', width: 320, height: 220 }])
 
     expect(Wrapper.find('div').length).toEqual(1)
     expect(Wrapper.find('img').length).toEqual(1)
     expect(Wrapper.find(LazyLoad).length).toEqual(1)
+  })
+
+  it('should render the loading animation when there is no data', () => {
+    const { Wrapper } = setup([])
+
+    expect(Wrapper.find(SpinnerContainer).length).toEqual(1)
+    expect(Wrapper.find(Spinner).length).toEqual(1)
   })
 })
--- test/VideosPage.test.js
+++ test/VideosPage.test.js
@@ -3,14 +3,16 @@ import expect from 'expect'
 import { shallow } from 'enzyme'
 import { VideosPage } from '../src/components/VideosPage'
 import LazyLoad from 'react-lazyload'
+import SpinnerContainer from '../src/components/StyledComponents/SpinnerContainer'
+import Spinner from '../src/components/StyledComponents/Spinner'
 
 
-const setup = () => {
+const setup = (videos) => {
   const props = {
     handleSearch: expect.createSpy(),
     dispatch: expect.createSpy(),
     ref: expect.createSpy(),
-    videos: [{ id: 1, mediaUrl: 'test video url', width: 230 }]
+    videos: videos
   }
 
   const Wrapper = shallow(<VideosPage {...props} />)
@@ -19,10 +21,16 @@ const setup = () => {
 
 describe('Test for VideosPage', () => {
   it('should render self and sub components', () => {
-    const { Wrapper } = setup()
+    const { Wrapper } = setup([{ id: 1, mediaUrl: 'test image url', width: 320, height: 220 }])
 
     expect(Wrapper.find('div').length).toEqual(1)
     expect(Wrapper.find('video').length).toEqual(1)
     expect(Wrapper.find(LazyLoad).length).toEqual(1)
   })
-})
+
+  it('should render the loading animation when there is no data', () => {
+    const { Wrapper } = setup([])
+
+    expect(Wrapper.find(SpinnerContainer).length).toEqual(1)
+    expect(Wrapper.find(Spinner).length).toEqual(1)
+  })})
