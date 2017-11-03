3b954f7f75a68dc170148e36ffb45fda994ecc33
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-11-03 15:13:13 +0100

    Amended API call to check the count of the photos and to define which page num to request if we assume our chunks are 100 photos per page.

--- src/API/api.js
+++ src/API/api.js
@@ -8,29 +8,25 @@ const REACT_APP_FLICKR_PHOTOSET_ID = process.env.REACT_APP_FLICKR_PHOTOSET_ID
 const REACT_APP_FLICKR_VIDEOSET_ID = process.env.REACT_APP_FLICKR_VIDEOSET_ID
 
 export async function flickrImages() {
-  const FLICKR_API_ENDPOINT = `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos
+  const PHOTOSET_INFO_ENDPOINT = `https://api.flickr.com/services/rest/?method=flickr.photosets.getInfo&api_key=${REACT_APP_FLICKR_API_KEY}&photoset_id=${REACT_APP_FLICKR_PHOTOSET_ID}&user_id=${REACT_APP_FLICKR_USER_ID}&format=json&nojsoncallback=1`
+
+  let fetchedInfo = await fetch(PHOTOSET_INFO_ENDPOINT)
+  fetchedInfo = await fetchedInfo.json()
+  const lastPageNum = Math.ceil(fetchedInfo.photoset.count_photos / 100) || 1
+
+  const GET_PHOTOS_ENDPOINT = `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos
 	&api_key=${REACT_APP_FLICKR_API_KEY}
 	&format=json
 	&nojsoncallback=1
 	&per_page=100
-	&page=5
+	&page=${lastPageNum}
 	&user_id=${REACT_APP_FLICKR_USER_ID}
 	&photoset_id=${REACT_APP_FLICKR_PHOTOSET_ID}
 	&extras=date_taken,o_dims,url_o`
 
-  let fetchedData = await fetch(FLICKR_API_ENDPOINT)
+  let fetchedData = await fetch(GET_PHOTOS_ENDPOINT)
   fetchedData = await fetchedData.json()
-  return fetchedData.photoset.photo.reverse().map(({
-                                                     farm,
-                                                     server,
-                                                     id,
-                                                     secret,
-                                                     title,
-                                                     url_o,
-                                                     height_o,
-                                                     width_o,
-                                                     datetaken
-                                                   }) => ({
+  return fetchedData.photoset.photo.reverse().map(({farm, server, id, secret, title, url_o, height_o, width_o, datetaken}) => ({
     id,
     title,
     mediaUrl: `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`,
@@ -53,7 +49,7 @@ export async function flickrVideos() {
 
   let fetchedData = await fetch(FLICKR_API_ENDPOINT)
   fetchedData = await fetchedData.json()
-  return fetchedData.photoset.photo.reverse().map(({ id, secret, title }) => ({
+  return fetchedData.photoset.photo.reverse().map(({id, secret, title}) => ({
     id,
     title,
     mediaUrl: `https://www.flickr.com/photos/${REACT_APP_FLICKR_USER_ID}/${id}/play/site/${secret}`
