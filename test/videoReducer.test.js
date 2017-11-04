2707e1c4d825276ba96e084800705a4bc477b2c7
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-11-04 10:54:50 +0100

    Updating tests.

--- test/videoReducer.test.js
+++ test/videoReducer.test.js
@@ -1,9 +1,9 @@
 /**
  * Created by mgab on 29/03/2017.
  */
-import expect from 'expect';
-import videoReducer from '../src/reducers/videoReducer';
-import * as types from '../src/constants/actionTypes';
+import expect from 'expect'
+import videoReducer from '../src/reducers/videoReducer'
+import * as types from '../src/constants/actionTypes'
 
 describe('Test for Video Reducer', () => {
   const initialState = {
@@ -11,20 +11,14 @@ describe('Test for Video Reducer', () => {
       id: 1,
       link: 'www.test.com/1.mp4'
     }]
-  };
+  }
 
   it('should return the initial state', () => {
-    expect(videoReducer(undefined, [])).toEqual([]);
-  });
+    expect(videoReducer(undefined, [])).toEqual([])
+  })
 
   it('should return the all videos in the stores tree', () => {
-    const testAction = { type: types.FLICKR_VIDEOS_SUCCESS, videos: 'www.test.com/1.mp4' };
-    expect(videoReducer(initialState, testAction)).toEqual([ 'www.test.com/1.mp4' ]);
-  });
-
-  it('should return the selected video', () => {
-    const testAction = { type: types.SELECTED_VIDEO, video: 'www.test.com/1.mp4' };
-    const expectValue = { videos: [{ id: 1, link: 'www.test.com/1.mp4' }], selectedVideo: 'www.test.com/1.mp4' };
-    expect(videoReducer(initialState, testAction)).toEqual(expectValue);
-  });
-});
+    const testAction = { type: types.FLICKR_VIDEOS_SUCCESS, videos: 'www.test.com/1.mp4' }
+    expect(videoReducer(initialState, testAction)).toEqual([ 'www.test.com/1.mp4' ])
+  })
+})
