43655cfa766973d3ef6f80b2d4a6b9c530064883
Mihail Gaberov <mihail.gaberov@gmail.com>
2018-03-31 17:38:16 +0200

    Formatting.

--- src/__tests__/PhotosPage.test.js
+++ src/__tests__/PhotosPage.test.js
@@ -15,12 +15,12 @@ const setup = (images, error) => {
   }
 
   const Wrapper = shallow(<PhotosPage {...props} />)
-  return {Wrapper, props}
+  return { Wrapper, props }
 }
 
 describe('PhotosPage component', () => {
   it('should render self and sub components', () => {
-    const {Wrapper} = setup([{id: 1, mediaUrl: 'test image url', width: 320, height: 220}])
+    const { Wrapper } = setup([{ id: 1, mediaUrl: 'test image url', width: 320, height: 220 }])
 
     expect(Wrapper.find('div').length).toEqual(1)
     expect(Wrapper.find('img').length).toEqual(1)
@@ -28,14 +28,14 @@ describe('PhotosPage component', () => {
   })
 
   it('should render the loading animation when there is no data', () => {
-    const {Wrapper} = setup([])
+    const { Wrapper } = setup([])
 
     expect(Wrapper.find(SpinnerContainer).length).toEqual(1)
     expect(Wrapper.find(Loader).length).toEqual(1)
   })
 
   it('should show error message if any errors occurred', () => {
-    const {Wrapper} = setup([], 'error')
+    const { Wrapper } = setup([], 'error')
     expect(Wrapper.find(ErrorMsg).length).toEqual(1)
   })
 })
