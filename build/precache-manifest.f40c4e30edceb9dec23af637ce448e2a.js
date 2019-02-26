0fae4113ec774da62e551532e2dc07710cdf3de3
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-02-26 11:14:34 +0100

    New release

---
+++ build/precache-manifest.f40c4e30edceb9dec23af637ce448e2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "75f2ba958b09dbe0786a",
+    "url": "/static/css/main.9a47a675.chunk.css"
+  },
+  {
+    "revision": "75f2ba958b09dbe0786a",
+    "url": "/static/js/main.75f2ba95.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d01615aade2ef6021d0a",
+    "url": "/static/css/2.1161ee37.chunk.css"
+  },
+  {
+    "revision": "d01615aade2ef6021d0a",
+    "url": "/static/js/2.d01615aa.chunk.js"
+  },
+  {
+    "revision": "e14874d19a4630c6cdd618a42e38295c",
+    "url": "/index.html"
+  }
+];
